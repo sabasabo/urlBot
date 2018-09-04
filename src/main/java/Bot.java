b399217a3d1979fc0d7590d6a2b7a96b2c141ff8
liron48290 <lir48290@gmail.com>
2018-09-04 20:08:12 +0300

    heroku

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -5,29 +5,48 @@ import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+import org.telegram.telegrambots.util.WebhookUtils;
 
 import java.util.Optional;
 
+
 public class Bot {
 
-    private static final String EXTERNAL_URL = System.getenv("HEROKU_URL") + System.getenv("TOKEN");
+    private static final String EXTERNAL_URL = "https://" + System.getenv("HEROKU_URL_PREFIX") + ".herokuapp.com/";
     private static final String INTERNAL_URL_AND_PORT = "https://0.0.0.0:" + Optional.ofNullable(System.getenv("PORT")).orElse("8080");
 
-    private static class webhookHandler extends TelegramWebhookBot {
+    private static class WebhookHandler extends TelegramWebhookBot {
 
         public BotApiMethod onWebhookUpdateReceived(Update update) {
             if (update.hasMessage() && update.getMessage().hasText()) {
+                System.out.println("received message");
                 SendMessage sendMessage = new SendMessage();
                 sendMessage.setChatId(update.getMessage().getChatId().toString());
                 String text = update.getMessage().getText();
-                sendMessage.setText(text.substring(0, Math.min(text.indexOf('?'), text.length())));
+                text = removeUrlParamters(text);
+                text = removePreHttpChars(text);
+                sendMessage.setText(text);
                 return sendMessage;
             }
             return null;
         }
 
+        private String removePreHttpChars(String text) {
+            if (!text.contains("http")) {
+                return text;
+            }
+            return text.substring(text.indexOf("http"));
+        }
+
+        private String removeUrlParamters(String text) {
+            if (!text.contains("?")) {
+                return text;
+            }
+            return text.substring(0, text.indexOf('?'));
+        }
+
         public String getBotUsername() {
-            return "urlRemoveParametersBot";
+            return System.getenv("BOT_USER_NAME");
         }
 
         public String getBotToken() {
@@ -35,7 +54,7 @@ public class Bot {
         }
 
         public String getBotPath() {
-            return EXTERNAL_URL;
+            return System.getenv("TOKEN");
         }
     }
 
@@ -44,6 +63,9 @@ public class Bot {
         System.out.println("INTERNAL_URL_AND_PORT " + INTERNAL_URL_AND_PORT);
         System.out.println("port should be " + System.getenv("PORT"));
         ApiContextInitializer.init();
-        new TelegramBotsApi(EXTERNAL_URL, INTERNAL_URL_AND_PORT).registerBot(new webhookHandler());
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi(EXTERNAL_URL, INTERNAL_URL_AND_PORT);
+        WebhookHandler bot = new WebhookHandler();
+        WebhookUtils.clearWebhook(bot);
+        telegramBotsApi.registerBot(bot);
     }
 }
